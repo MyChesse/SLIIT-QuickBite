a836187080539c995d4b5f85bef40a6759dd26be
Isiwari Thimethya <isiwarithimethya@gmail.com>
2026-03-22 14:03:32 +0530

    Added backend folder to isiwari branch

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+connectDB();
+
+app.listen(PORT, () => {
+    console.log("Server is running on Port: ", PORT);});
+
